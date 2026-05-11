3a5a5fb03c941f85ab1492eb25c0ef22dbdaf31a
Aman Sharma <sharma9917375201@gmail.com>
2026-05-11 23:30:03 +0530

    create sign-in method

--- Complete-Auth/src/routes/auth.routes.js
+++ Complete-Auth/src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { register, getMe, refreshToken, logout } from "../controllers/auth.controller.js";
+import { register, getMe, refreshToken, logout, login } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
 authRouter.post("/register", register)
+authRouter.post("/login", login)
 authRouter.get("/get-me", getMe)
 authRouter.post("/refresh-token", refreshToken)
 authRouter.post("/logout", logout)
