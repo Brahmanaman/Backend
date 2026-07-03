8629993e8a61897e783f4e1dcdaa7b7074d8079e
Aman Sharma <sharma9917375201@gmail.com>
2026-07-03 23:43:43 +0530

    feat: update static file serving and implement voting interface with socket.io

--- Day21-Vote/backend/src/app.js
+++ Day21-Vote/backend/src/app.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url"
 
 const app = express()
 const _dirname = path.dirname(fileURLToPath(import.meta.url))
-app.use(express.static(_dirname + "/public"))
+app.use(express.static(path.join(_dirname, "..", "public")))
 
 
 app.get("/", (req, res) => {
--- Day21-Vote/backend/src/sockets/socket.server.js
+++ Day21-Vote/backend/src/sockets/socket.server.js
@@ -1,25 +1,31 @@
-import { server } from "socket.io"
+import { Server } from "socket.io"
 
-voteCounts = {
+let voteCounts = {
     yes: 0,
     no: 0
 }
 
+const socketsids = []
+
 export default function initSocket(httpserver) {
-    const io = new server(httpserver)
+    const io = new Server(httpserver)
     io.on("connection", (socket) => {
         console.log("user connected")
 
         socket.on("vote_yes", () => {
+            if (socketsids.includes(socket.id)) return;
+            socketsids.push(socket.id)
             voteCounts.yes += 1;
             io.emit("vote_updated", voteCounts);
         })
 
-        socket.on("vote_no", () =>{
+        socket.on("vote_no", () => {
+            if (socketsids.includes(socket.id)) return;
+            socketsids.push(socket.id)
             voteCounts.no += 1;
             io.emit("vote_updated", voteCounts);
         })
-        
+
         socket.on("disconnect", () => {
             console.log("user disconnected")
         })
